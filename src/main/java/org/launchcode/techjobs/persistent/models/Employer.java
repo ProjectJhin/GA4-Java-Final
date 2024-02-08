389c43beb3c91e911988989f3dc77053b3e5d0f0
ProjectJhin <zijianw1000@gmail.com>
2024-02-08 15:53:49 -0600

    v1 - test 1 and 2 complete

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,40 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany(mappedBy = "employer")
+    private final List<Job> jobs = new ArrayList<>();
+
+    @NotBlank
+    @Size(min = 2, max = 50, message="Location name must be between 2 and 50 characters")
+    private String location;
+
+    public Employer(String location) {
+        super();
+        this.location = location;
+    }
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotBlank
+    @Size(min=1, max = 300, message="Skill description must be less than 300 characters")
+    private String description;
+
+    public Skill(String description) {
+        super();
+        this.description = description;
+    }
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
